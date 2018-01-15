b52bab7876e88c97e4a2c382714b151d275f76b2
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2018-01-15 19:59:51 -0400

    Componente media.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hola Mundo!')
+import React from 'react';
+import { render } from 'react-dom';
+import Media from './src/playlist/componentes/media';
+
+//console.log('Hola Mundo!')
+
+const app = document.getElementById('app')
+
+//ReactDOM.render(que voy a renderizar, dónde lo haré)
+//const holaMundo = <h1>Hola Mundo!!!!</h1>;
+render(<Media />, app);
