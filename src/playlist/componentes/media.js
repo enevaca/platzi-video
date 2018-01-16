8aad05724cecfa0cdae8f9f2f48e246750ae02cf
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2018-01-15 20:38:26 -0400

    Estilos en React

--- src/playlist/componentes/media.js
+++ src/playlist/componentes/media.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
+import './media.css'
 
 class Media extends Component {
 	render() {
+		const styles = {
+			container: {
+				color: '#44546b',
+				cursor: 'pointer',
+				width: 260,
+				border: '1px solid red'
+			}
+		}
 		return (
-			<div>
-				<div>
-					<img 
-						scr="" 
-						alt="" 
-						width={260} 
-						height={160} 
+			<div className="Media">
+				<div className="Media-cover">
+					<img
+			            src="./images/covers/bitcoin.jpg"
+			            alt=""
+			            width={260}
+			            height={160}
+			            className="Media-image"
 					/>
-					<h3>¿Por qué aprender React?...</h3>
-					<p>Noel Vaca Moreno!</p>
+					<h3 className="Media-title">¿Por qué aprender React?...</h3>
+					<p className="Media-author">Noel Vaca Moreno!</p>
 				</div>
 			</div>
 		)
