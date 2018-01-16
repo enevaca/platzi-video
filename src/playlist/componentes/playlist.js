8e5754babe25567b429f904fd18ff32c9baadccc
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2018-01-16 19:32:44 -0400

    Composición de componentes

--- src/playlist/componentes/playlist.js
+++ src/playlist/componentes/playlist.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css';
+import Play from '../../icons/components/play'
 
 // function Playlist(props) {
 // 	return (
@@ -15,6 +16,10 @@ function Playlist (props) {
 	console.log(props.data)
 	return (
 		<div className="Playlist">
+			<Play 
+				size={100}
+				color="red"
+			/>
 			{
 				playlist.map((item) => {
 					return <Media {...item} key={item.id} />
