b0da9cce5496b9be306338a3e93e67c9b637de9a
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2018-01-17 16:01:17 -0400

    Estructura del proyecto con Containers y UI components: layout de nuestro sitio web

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Playlist from './src/playlist/componentes/playlist';
-import data from './src/api.json'
+import Home from '../pages/containers/home'
+//import Playlist from './src/playlist/componentes/playlist';
+//import data from './src/api.json'
 
 //console.log('Hola Mundo!')
 
@@ -9,4 +10,4 @@ const app = document.getElementById('app')
 
 //ReactDOM.render(que voy a renderizar, dónde lo haré)
 //const holaMundo = <h1>Hola Mundo!!!!</h1>;
-render(<Playlist data={data} />, app);
+render(<Home />, app);
