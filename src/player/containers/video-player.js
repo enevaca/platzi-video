d5912ec60bbf301cde389e3ad2630d36e8294532
Noel Vaca Moreno <noel.vm.2501@gmail.com>
2018-01-18 16:41:27 -0400

    Play / Pausa: Creando la UI

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+
+    state = {
+        pause: true,
+    }
+
+    toggleClick = (event) => {
+        this.setState({
+            pause: !this.state.pause
+        })
+    }
+
     render() {
         return (
             <VideoPlayerLayout>
                 <Title 
                     title="Esto es un video chido!"
                 />
+                <PlayPause 
+                    pause={this.state.pause}
+                    handleClick={this.toggleClick}
+                />
                 <Video
-                    autoplay={false}
+                    autoplay={true}
                     src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
                 />
             </VideoPlayerLayout>
